e9547822ecf43f6befdb05351d80d60d5f7f4da9
ferried <harlancui@outlook.com>
2019-07-26 17:01:31 +0800

    浅草才能磨马蹄

--- src/extension.ts
+++ src/extension.ts
@@ -1,27 +1,18 @@
 // The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
 
-// this method is called when your extension is activated
-// your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "hacpi-cr" is now active!');
-
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('extension.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World!');
+	let hacpicr = vscode.commands.registerCommand('extension.hacpicr', () => {
+		vscode.window.showInformationMessage("hacpiLogin");
 	});
 
-	context.subscriptions.push(disposable);
-}
+	let window = vscode.commands.registerCommand('hacpi-cr', () => {
+		console.log('1111');
+	});
+	
+	context.subscriptions.push(hacpicr);
+	context.subscriptions.push(window);
 
-// this method is called when your extension is deactivated
-export function deactivate() {}
+}
+export function deactivate() { }
